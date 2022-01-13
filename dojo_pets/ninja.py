aecaff3f3e23eada0075c1c495d064abd1fcaf71
Geetha Medisetty <geetha.medisetty@gmail.com>
2022-01-13 00:28:09 -0800

    dojo pets assignment

---
+++ dojo_pets/ninja.py
@@ -0,0 +1,14 @@
+class Ninja:
+    def __init__(self,first_name , last_name , treats , pet_food , pet):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.treats=treats
+        self.pet_food=pet_food
+        self.pet=pet
+
+    # implement the following methods:
+    # walk() - walks the ninja's pet invoking the pet play() method
+    def walk(self):
+        print("hhj")
+    # feed() - feeds the ninja's pet invoking the pet eat() method
+    # bathe() - cleans the ninja's pet invoking the pet noise() method
